c7f7b33dd3e0aad8a07cd5db88fc3eef7e4b3eb8
Yoav <yoavklein25@gmail.com>
2024-03-06 21:11:48 +0200

    new lesson script

---
+++ New-Lesson.ps1
@@ -0,0 +1,7 @@
+
+$Latest = 0
+
+$Max = $(Get-ChildItem -Directory).Name | Foreach-Object { "$_" -replace '\D+(\d+)', '$1' }  | Measure-Object -Maximum | Select-Object -Property 'Maximum'
+$MaxInt = [int]$Max.Maximum
+
+Copy-Item -Recurse "lesson_$MaxInt" "lesson_$($MaxInt + 1)"
